b111e9c712c311e7c91862d9d90f2797a3987f0c
Suresh8520 <108584442+Suresh8520@users.noreply.github.com>
2023-04-02 18:51:04 +0530

    Update main.tf

--- Topics/1.4 Count/main.tf
+++ Topics/1.4 Count/main.tf
@@ -1,4 +1,21 @@
 # Terraform Block
+
+variable "bucket_names" {
+  type = list(string)
+  default = ["bucket1", "bucket2"]
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  count = length(var.bucket_names)
+  bucket = var.bucket_names[count.index]
+  acl    = "private"
+  
+  tags = {
+    Name = "My Bucket ${count.index + 1}"
+  }
+}
+
+
 terraform {
   required_providers {
     aws = {
